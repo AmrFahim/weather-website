923abb019084cf3bf50e9e38f9cf945bc6be2730
AmrFahim <amrmf2020@gmail.com>
2020-04-26 15:22:52 +0200

    Init commit

---
+++ src/utils/forcast.js
@@ -0,0 +1,18 @@
+request = require('request')
+const forcast = (latitude, longtitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=7369ce3309fd7f5ae99abe368e7f5331&query=' + latitude + ',' + longtitude +
+        '&units=m'
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect weather therweather service')
+        } else if (body.error) {
+            callback('Unable to find the location, try another one')
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. The temperature is ' + body.current.temperature +
+                ' and it\'s feel like ' + body.current.feelslike
+            )
+        }
+    })
+}
+
+module.exports = forcast
